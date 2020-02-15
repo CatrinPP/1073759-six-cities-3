ed820a4d99d00eb076dd68ae6a39f62a21b757bb
CatrinPP <caterina.parshina@gmail.com>
2020-02-16 01:35:08 +0300

    Создает моки для приложения и для тестов

---
+++ src/mocks/offers.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    image: `img/room.jpg`,
+    isPremium: false,
+    price: 98,
+    rating: 90,
+    title: `Amazing Studio Loft`,
+    type: `Apartment`,
+  }, {
+    image: `img/apartment-01.jpg`,
+    isPremium: false,
+    price: 34,
+    rating: 80,
+    title: `Big Vintage Room`,
+    type: `Room`,
+  }, {
+    image: `img/apartment-02.jpg`,
+    isPremium: false,
+    price: 70,
+    rating: 85,
+    title: `Small House in the city center`,
+    type: `House`,
+  }, {
+    image: `img/apartment-03.jpg`,
+    isPremium: true,
+    price: 320,
+    rating: 95,
+    title: `Villa with amazing view`,
+    type: `Villa`,
+  },
+];
