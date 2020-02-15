4de4c2e86f7e591c2d38da9e512b50fbd3bbbbbd
CatrinPP <caterina.parshina@gmail.com>
2020-02-16 01:44:46 +0300

    Подключает моки в точку входа

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
+import offers from './mocks/offers.js';
 
 const Settings = {
   PLACES_TO_STAY_COUNT: 312
 };
 
-const TITLES = [
-  `Beautiful & luxurious apartment at great location`,
-  `Wood and stone place`,
-  `Canal View Prinsengracht`,
-  `Nice, cozy, warm big bed apartment`,
-  `Wood and stone place`
-];
-
 ReactDOM.render(
     <App
-      offerTitles={TITLES}
+      offers={offers}
       placesToStayCount={Settings.PLACES_TO_STAY_COUNT}
     />,
     document.querySelector(`#root`)
