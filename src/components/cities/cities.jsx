7b6f75d19f2acbe3fd8060e3adca58d04e216394
CatrinPP <caterina.parshina@gmail.com>
2020-02-29 18:05:00 +0300

    Ограниченно реализует переключение города

--- src/components/cities/cities.jsx
+++ src/components/cities/cities.jsx
@@ -5,18 +5,34 @@ import {offersListShape} from '../../const.js';
 class Cities extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentCity: props.offers[0].city,
+    };
+  }
+
+  handleCityClick(city) {
+    const onCityClick = () => {
+      this.setState({
+        currentCity: city
+      });
+    };
+    return onCityClick;
   }
 
   render() {
     const {offers} = this.props;
-    const offersToSHow = offers.slice(0, 6);
+    const {currentCity} = this.state;
+    const offersToShow = offers.slice(0, 6);
     return (
       <ul className="locations__list tabs__list">
-        {offersToSHow.map((it) => (
+        {offersToShow.map((it) => (
           <li className="locations__item"
             key={it.city}
           >
-            <a className={`locations__item-link tabs__item ${(it.city === offersToSHow[0].city) && `tabs__item--active`} href="#"`}>
+            <a className={`locations__item-link tabs__item ${(it.city === currentCity) && `tabs__item--active`} href="#"`}
+              onClick={this.handleCityClick(it.city)}
+            >
               <span>{it.city}</span>
             </a>
           </li>
