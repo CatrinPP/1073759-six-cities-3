638b9148eb3135e0715cc3055e4753a9caab6273
CatrinPP <caterina.parshina@gmail.com>
2020-03-06 20:23:25 +0300

    Удаляет комментарии

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
-// import offers from './mocks/offers.js';
 import {reducer} from './reducer.js';
 
 const store = createStore(
@@ -13,9 +12,7 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-      <App
-        // offers={offers}
-      />
+      <App />
     </Provider>,
     document.querySelector(`#root`)
 );
