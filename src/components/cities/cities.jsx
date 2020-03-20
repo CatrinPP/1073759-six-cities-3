9fb65a78e3ab64baa4050743c1444f5bc200350a
CatrinPP <caterina.parshina@gmail.com>
2020-03-20 11:46:20 +0300

    Переписывает Список городов в функциональный компонент

--- src/components/cities/cities.jsx
+++ src/components/cities/cities.jsx
@@ -1,33 +1,30 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {offersListShape, cityShape} from '../../const.js';
 import {ActionCreator} from '../../reducer.js';
 
-class Cities extends PureComponent {
-  render() {
-    const {allOffers, city, handleCityClick} = this.props;
-    const offersToShow = allOffers.slice(0, 6);
+const Cities = ({allOffers, city, handleCityClick}) => {
+  const offersToShow = allOffers.slice(0, 6);
 
-    const onCityNameClick = (newCity) => () => handleCityClick(newCity);
+  const handleCityNameClick = (newCity) => () => handleCityClick(newCity);
 
-    return (
-      <ul className="locations__list tabs__list">
-        {offersToShow.map((it) => (
-          <li className="locations__item"
-            key={it.city.name}
+  return (
+    <ul className="locations__list tabs__list">
+      {offersToShow.map((it) => (
+        <li className="locations__item"
+          key={it.city.name}
+        >
+          <a className={`locations__item-link tabs__item ${it.city.name === city.name ? `tabs__item--active` : ``}`} href="#"
+            onClick={handleCityNameClick(it.city)}
           >
-            <a className={`locations__item-link tabs__item ${it.city.name === city.name ? `tabs__item--active` : ``}`} href="#"
-              onClick={onCityNameClick(it.city)}
-            >
-              <span>{it.city.name}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+            <span>{it.city.name}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 Cities.propTypes = {
   allOffers: PropTypes.arrayOf(PropTypes.shape(offersListShape)).isRequired,
