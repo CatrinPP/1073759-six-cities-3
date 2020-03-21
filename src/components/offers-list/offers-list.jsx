d2e989329dad394e0a37f92757f946d842be2a8c
CatrinPP <caterina.parshina@gmail.com>
2020-03-21 19:56:31 +0300

    Выносит состояние из компонента сортировки в hoc

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -6,9 +6,11 @@ import Offer from '../offer/offer.jsx';
 import Sorting from '../sorting/sorting.jsx';
 import {offerShape, cityShape} from '../../const.js';
 import {sortOffers} from '../../utils.js';
+import withShowControl from '../../hocs/with-show-control.js';
 
 const OffersList = ({city, handlePlaceCardHover, handlePlaceCardNameClick, handleSortTypeClick, isCitiesClass, offers, sortType}) => {
   const sortedOffers = [...offers];
+  const SortingWrapped = withShowControl(Sorting);
 
   const handleCardNameClick = (newOffer) => () => handlePlaceCardNameClick(newOffer);
   const handleCardHover = (newOffer) => () => handlePlaceCardHover(newOffer);
@@ -30,7 +32,7 @@ const OffersList = ({city, handlePlaceCardHover, handlePlaceCardNameClick, handl
           <React.Fragment>
             <h2 className="visually-hidden">Places</h2>
             <b className="places__found">{sortedOffers.length} places to stay in {city.name}</b>
-            <Sorting
+            <SortingWrapped
               sortType={sortType}
               handleSortTypeClick={handleSortTypeClick}
             />
--- src/components/sorting/sorting.jsx
+++ src/components/sorting/sorting.jsx
@@ -1,48 +1,39 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {sortTypes} from '../../const.js';
 
-export default class Sorting extends PureComponent {
-  constructor(props) {
-    super(props);
+const Sorting = ({handleShowUpClick, handleSortTypeClick, isCollapsed, sortType}) => {
+  const handleTypeClick = (selectedSortType) => () => handleSortTypeClick(selectedSortType);
 
-    this.state = {
-      isCollapsed: true,
-    };
-  }
-
-  render() {
-    const {handleSortTypeClick, sortType} = this.props;
-    const handleTypeClick = (selectedSortType) => () => handleSortTypeClick(selectedSortType);
-
-    return (
-      <form className="places__sorting" action="#" method="get">
-        <span className="places__sorting-caption">Sort by &nbsp;</span>
-        <span className="places__sorting-type"
-          onClick={() => {
-            this.setState({isCollapsed: !this.state.isCollapsed});
-          }}
-          tabIndex="0">
-          {sortType}
-          <svg className="places__sorting-arrow" width="7" height="4">
-            <use xlinkHref="#icon-arrow-select"></use>
-          </svg>
-        </span>
-        <ul className={`places__options places__options--custom ${this.state.isCollapsed ? `` : `places__options--opened`}`}>
-          {sortTypes.map((it) => (
-            <li className={`places__option ${it === sortType ? `places__option--active` : ``}`}
-              key={it}
-              onClick={handleTypeClick(it)}
-              tabIndex="0">{it}
-            </li>
-          ))}
-        </ul>
-      </form>
-    );
-  }
-}
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by &nbsp;</span>
+      <span className="places__sorting-type"
+        onClick={handleShowUpClick}
+        tabIndex="0">
+        {sortType}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className={`places__options places__options--custom ${isCollapsed ? `` : `places__options--opened`}`}>
+        {sortTypes.map((it) => (
+          <li className={`places__option ${it === sortType ? `places__option--active` : ``}`}
+            key={it}
+            onClick={handleTypeClick(it)}
+            tabIndex="0">{it}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+};
 
 Sorting.propTypes = {
+  handleShowUpClick: PropTypes.func.isRequired,
   handleSortTypeClick: PropTypes.func.isRequired,
+  isCollapsed: PropTypes.bool.isRequired,
   sortType: PropTypes.string.isRequired,
 };
+
+export default Sorting;
