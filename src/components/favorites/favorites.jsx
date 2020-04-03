2a9f645a5ab72f915c7aca9ceec0f971a47453fc
CatrinPP <caterina.parshina@gmail.com>
2020-04-03 19:40:18 +0300

    дорабатывает компонент избранного

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -1,143 +1,107 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 import Header from '../header/header.jsx';
+import {getSortedFavorites} from '../../reducer/user/selectors.js';
+import {connect} from 'react-redux';
+import {Operation as UserOperation} from '../../reducer/user/user.js';
+import {Operation as DataOperation} from '../../reducer/data/data.js';
+import {favoritesShape, PlaceCardType} from '../../const.js';
+import Offer from '../offer/offer.jsx';
 
-const Favorites = () => {
-  return (
-    <div classNameName="page">
-      <Header />
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>Amsterdam</span>
-                    </a>
-                  </div>
-                </div>
-                <div className="favorites__places">
-                  <article className="favorites__card place-card">
-                    <div className="favorites__image-wrapper place-card__image-wrapper">
-                      <a href="#">
-                        <img className="place-card__image" src="img/apartment-small-03.jpg" width="150" height="110" alt="Place image"/>
-                      </a>
-                    </div>
-                    <div className="favorites__card-info place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;180</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">In bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: `100%`}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <a href="#">Nice, cozy, warm big bed apartment</a>
-                      </h2>
-                      <p className="place-card__type">Apartment</p>
-                    </div>
-                  </article>
+class Favorites extends PureComponent {
+  componentDidMount() {
+    const {getFavoriteOffers} = this.props;
+    getFavoriteOffers();
+  }
 
-                  <article className="favorites__card place-card">
-                    <div className="favorites__image-wrapper place-card__image-wrapper">
-                      <a href="#">
-                        <img className="place-card__image" src="img/room-small.jpg" width="150" height="110" alt="Place image"/>
-                      </a>
-                    </div>
-                    <div className="favorites__card-info place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;80</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">In bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: `80%`}}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <a href="#">Wood and stone place</a>
-                      </h2>
-                      <p className="place-card__type">Private room</p>
-                    </div>
-                  </article>
-                </div>
-              </li>
+  render() {
+    const {favorites, handleBookmarkButtonClick, handlePlaceCardNameClick} = this.props;
 
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>Cologne</span>
-                    </a>
-                  </div>
+    const handleButtonClick = (offer) => () => handleBookmarkButtonClick(offer);
+    const handleCardNameClick = (offer) => () => handlePlaceCardNameClick(offer);
+
+    return (
+      <div className={`page ${!favorites.length ? ` page--favorites-empty` : ``}`}>
+        <Header />
+        <main className={`page__main page__main--favorites ${!favorites.length ? ` page__main--favorites-empty` : ``}`}>
+          <div className="page__favorites-container container">
+            {!favorites.length ? (
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan yor future trips.</p>
                 </div>
-                <div className="favorites__places">
-                  <article className="favorites__card place-card">
-                    <div className="favorites__image-wrapper place-card__image-wrapper">
-                      <a href="#">
-                        <img className="place-card__image" src="img/apartment-small-04.jpg" width="150" height="110" alt="Place image"/>
-                      </a>
-                    </div>
-                    <div className="favorites__card-info place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;180</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
+              </section>
+            )
+              :
+              <section className="favorites">
+                <h1 className="favorites__title">Saved listing</h1>
+                <ul className="favorites__list">
+                  {favorites.map((it) => {
+                    return (
+                      <li className="favorites__locations-items" key={it.city}>
+                        <div className="favorites__locations locations locations--current">
+                          <div className="locations__item">
+                            <a className="locations__item-link" href="#">
+                              <span>{it.city}</span>
+                            </a>
+                          </div>
                         </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"></use>
-                          </svg>
-                          <span className="visually-hidden">In bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={{width: `100%`}}></span>
-                          <span className="visually-hidden">Rating</span>
+                        <div className="favorites__places">
+                          {it.offers.map((offer) => (
+                            <Offer
+                              key={offer.id}
+                              offer={offer}
+                              placeCardType={PlaceCardType.FAVORITES}
+                              handleBookmarkButtonClick={handleButtonClick(offer)}
+                              handlePlaceCardNameClick={handleCardNameClick(offer)}
+                            />
+                          ))}
                         </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <a href="#">White castle</a>
-                      </h2>
-                      <p className="place-card__type">Apartment</p>
-                    </div>
-                  </article>
-                </div>
-              </li>
-            </ul>
-          </section>
-        </div>
-      </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
-        </a>
-      </footer>
-    </div>
-  );
+                      </li>
+                    );
+                  })}
+                </ul>
+              </section>
+            }
+          </div>
+        </main>
+        <footer className="footer container">
+          <a className="footer__logo-link" href="main.html">
+            <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+          </a>
+        </footer>
+      </div>
+    );
+  }
+}
+
+Favorites.propTypes = {
+  favorites: PropTypes.arrayOf(PropTypes.shape(favoritesShape).isRequired).isRequired,
+  getFavoriteOffers: PropTypes.func.isRequired,
+  handleBookmarkButtonClick: PropTypes.func.isRequired,
+  handlePlaceCardNameClick: PropTypes.func.isRequired,
 };
 
-export default Favorites;
+const mapStateToProps = (state) => ({
+  favorites: getSortedFavorites(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getFavoriteOffers() {
+    dispatch(UserOperation.getFavorites());
+  },
+
+  handleBookmarkButtonClick(offer) {
+    dispatch(DataOperation.toggleIsFavorite(offer));
+    dispatch(UserOperation.getFavorites());
+  },
+
+  handlePlaceCardNameClick(offer) {
+    dispatch(DataOperation.openDetailedOffer(offer));
+  },
+});
+
+export {Favorites};
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
